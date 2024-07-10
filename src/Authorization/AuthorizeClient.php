023026d9d88334dad7cebdb6fc94fe10ed5e5b22
mhofmann <m.hofmann@web-vision.de>
2024-07-11 01:13:36 +0200

    [TASK] Add Bearer Auth and fetch user
    Adds the token auth and adds the user as attribute to the current request

--- src/Authorization/AuthorizeClient.php
+++ src/Authorization/AuthorizeClient.php
@@ -4,14 +4,24 @@ declare(strict_types=1);
 
 namespace Capito\Authorization;
 
+use Capito\DatabaseTrait;
+use Capito\Model\User;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Http\ServerRequest;
+use Slim\Psr7\Factory\ResponseFactory;
+use Slim\Psr7\Factory\StreamFactory;
 
 class AuthorizeClient implements MiddlewareInterface
 {
+    use DatabaseTrait;
+
+    private const array IGNORE_VALIDATION = [
+        '/v2/account',
+        '/v2/account/verify',
+    ];
     /**
      * @param ServerRequest $request
      * @param RequestHandlerInterface $handler
@@ -19,17 +29,68 @@ class AuthorizeClient implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        /**
-         * @ignore-vaidation /v2/account
-         * @ignore-validation /v2/account/verify
-         */
-        // @todo add Bearer Auth here and return 401 or 403, if needed
+        $apiPath = $request->getRequestTarget();
+        if (!in_array($apiPath, self::IGNORE_VALIDATION, true)) {
+            $authHeader = $request->getHeader('Authorization')[0] ?? '';
+            if (!str_starts_with($authHeader[0], 'Bearer ')) {
+                return $this->createUnauthorizedResponse();
+            }
+
+            $token = substr($authHeader, strlen('Bearer '));
+            if (strlen($token) === 0) {
+                return $this->createUnauthorizedResponse();
+            }
+
+            $queryBuilder = $this->connection->createQueryBuilder();
+            $query = $queryBuilder
+                ->select('users.*', 'DISTINCT GROUP_CONCAT(roles.role) AS role')
+                ->from('users')
+                ->join(
+                    'users',
+                    'users_roles',
+                    'users_roles',
+                    'users.account_id=users_roles.account_id'
+                )
+                ->join(
+                    'users_roles',
+                    'roles',
+                    'roles',
+                    'users_roles.role=roles.id'
+                )
+                ->where($queryBuilder->expr()->eq('users.token', $queryBuilder->createNamedParameter($token)))
+                ->setMaxResults(1);
+            $row = $query->executeQuery()->fetchAssociative();
+            if ($row === false) {
+                return $this->createUnauthorizedResponse();
+            }
+
+            $roles = explode(',', $row['role']);
+            $user = new User(
+                $row['email'],
+                $row['password'],
+                $row['account_id'],
+                $row['id'],
+                in_array('admin', $roles, true),
+                in_array('member', $roles, true),
+                $row['is_verified']
+            );
+            $request = $request->withAttribute('user', $user);
+        }
+
         return $handler->handle($request);
-        /*
-         * 401: {
-  "property": "header::Authorization",
-  "message": "Authorization-Header either missing or empty"
-}
-         */
+    }
+
+    private function createUnauthorizedResponse(): ResponseInterface
+    {
+        return (new ResponseFactory())
+            ->createResponse(401, 'Unauthorized')
+            ->withHeader('Content-Type', 'application/json')
+            ->withBody(
+                (new StreamFactory())
+                    ->createStream(json_encode([
+                        'property' => 'header::Authorization',
+                        'message' => 'Authorization-Header either missing or empty'
+                    ]))
+            );
     }
 }
