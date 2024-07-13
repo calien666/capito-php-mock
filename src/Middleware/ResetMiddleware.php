c84ecdf4847c74b12419b575cec412bfde43cc3a
mhofmann <m.hofmann@web-vision.de>
2024-07-14 00:41:29 +0200

    [TASK] Add reset after every call

--- src/Middleware/ResetMiddleware.php
+++ src/Middleware/ResetMiddleware.php
@@ -6,16 +6,17 @@ namespace Capito\Middleware;
 
 use Capito\Database\ResetDatabase;
 use Psr\Http\Message\ServerRequestInterface;
-use Slim\Http\Interfaces\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+
 
 class ResetMiddleware
 {
     public function __invoke(
         ServerRequestInterface $request,
-        ResponseInterface $response,
-        callable $next = null
-    ) {
-        $response = $next($request, $response);
+        RequestHandlerInterface $handler
+    ): ResponseInterface {
+        $response = $handler->handle($request);
         ResetDatabase::reset();
         return $response;
     }
